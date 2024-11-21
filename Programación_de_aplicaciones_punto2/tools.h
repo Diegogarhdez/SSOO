67d481dad99f7f790a0d6447a523e93d9efb34f0
usuario <alu0101633732@ull.edu.es>
2024-11-21 17:59:08 +0000

    actu

---
+++ Programación_de_aplicaciones_punto2/tools.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cerrno>
+#include <cstring>
+#include <fstream>
+#include <expected>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <format>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include "SafeMap.h"
+
+#ifndef TOOLS_H_
+#define TOOLS_H_
+
+enum class parse_args_errors {
+  missing_argument,
+  unknown_option,
+  no_access_file,
+  no_file_exists,
+};
+
+struct program_options {
+  bool show_help = false;
+  bool modo_ampliado = false;
+  bool puerto = false;
+  std::string nombre_fichero;
+  std::string nombre_puerto;
+  std::vector<std::string> additional_args; 
+};
+
+
+std::expected<program_options, parse_args_errors> parse_args(int argc, char* argv[]);
+void Uso(const std::string&);
+void MostrarAyuda(const std::string&);
+void send_response(std::string_view header, std::string_view body = {});
+std::expected<SafeMap, int> read_all(const std::string& path);
+
+
+#endif
